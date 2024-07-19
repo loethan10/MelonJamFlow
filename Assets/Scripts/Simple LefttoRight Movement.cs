8800af173c08fe6be3d1bf2b19f967b37aa79bf7
Ethan Lo <loethan101@gmail.com>
2024-07-19 00:52:57 -0700

    The Big Bang
    This is literally the thing that starts it all bruhv, so leave me alone guatafa

---
+++ Assets/Scripts/Simple LefttoRight Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLefttoRightMovement : MonoBehaviour
+{
+    public float speed = 2;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Transform myTransform = transform;
+        transform.Translate(new Vector3(2,0,0) * speed * Time.deltaTime);
+ 
+    }
+}
