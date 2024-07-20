266f62a463beaaa51ef30fc66cd80a5c14b7a50c
Ethan Lo <loethan101@gmail.com>
2024-07-19 19:54:11 -0700

    Fix?
    FIx?

---
+++ Assets/Scripts/Simple RIghtLeft.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRIghtLeft : MonoBehaviour
+{
+    public float speed = 135;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Transform myTransform = transform;
+        transform.Translate(new Vector3(-2,0,0) * speed * Time.deltaTime);
+ 
+    }
+}
+
---
+++ Assets/Scripts/Typer.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typer : MonoBehaviour
+{
+    public Text wordOutput = null;
+    private string remainingWord = string.Empty;
+    private string currentWord = "i want to buy the muffins. the muffins tasted good. " +
+    "they tasted like blueberries. my mom killed herself after the divorce.";
+
+    int removedCount = 0;
+    int initialLength = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord(){
+        //Get bank word
+        SetRemainingWord(currentWord);
+        initialLength = currentWord.Length;
+        removedCount = 0;
+    }
+
+    private void SetRemainingWord(string newString){
+        remainingWord = newString;
+        wordOutput.text = remainingWord;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput(){
+        if(Input.anyKeyDown){
+            string keysPressed = Input.inputString;
+            if (keysPressed.Length == 1){
+                EnterLetter(keysPressed); 
+            }
+        }
+    }
+
+    private void EnterLetter(string typedLetter){
+        if(IsCorrectLetter(typedLetter)){
+            RemoveLetter();
+            if(IsWordComplete()){
+                SetCurrentWord(); 
+            }
+        }
+    }
+
+    private bool IsCorrectLetter(string letter){
+        return remainingWord.IndexOf(letter, removedCount) == 0+removedCount;
+    }
+
+    private void RemoveLetter(){
+        string newString = remainingWord.Remove(0+removedCount,1); 
+        SetRemainingWord(" " + newString);
+        removedCount++;
+    }
+
+    private bool IsWordComplete(){
+        return removedCount == initialLength;
+    }
+}
