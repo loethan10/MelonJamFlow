09a9e4dc34798d21d82915e50a654e2f7defe667
tomcab1 <tomcabrera16@icloud.com>
2024-07-21 13:49:19 -0700

    Push it real good, push it
    I accidentally killed word bank. Oh my whoospicles

---
+++ Assets/Scripts/Button Scripts/ObjectVisibilityToggle11.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ObjectVisibilityToggle11 : MonoBehaviour
+{
+    public GameObject objectToToggle;
+    //public float toggleDistance = 3f;
+    public bool objectVisible = false;
+    public WordBank wordBank = null;
+    public bool showButton = false;
+
+    private void Start()
+    {
+        objectToToggle.SetActive(false); // Set object initially invisible
+    }
+
+    private void Update()
+    {
+        if (wordBank.level11Done && objectToToggle != null)
+        {
+            ToggleObjectVisibility();
+        }
+    }
+
+    public void ToggleObjectVisibility()
+    {
+        if (!objectVisible || wordBank.level11Done)
+        {
+            Vector3 playerPosition = transform.position;
+            Vector3 objectPosition = objectToToggle.transform.position;
+            float distance = Vector3.Distance(playerPosition, objectPosition);
+
+            if (true)
+            {
+                objectVisible = true;
+                objectToToggle.SetActive(true); // Set object visible
+            }
+        } else {
+            Vector3 playerPosition = transform.position;
+            Vector3 objectPosition = objectToToggle.transform.position;
+            float distance = Vector3.Distance(playerPosition, objectPosition);
+
+            if (true)
+            {
+                objectVisible = false;
+                objectToToggle.SetActive(false); // Set object visible
+            }
+        }
+    }
+}
