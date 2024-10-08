5c498d4c37d15b83d285b9755e140ef49465097c
Ethan Lo <loethan101@gmail.com>
2024-10-07 21:17:50 -0700

    Shrug
    SHrug

---
+++ Assets/Scenes/ChangeScenes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public void GoToSceneTwo()
+    {
+        SceneManager.LoadScene("Level1");
+        
+    }
+}
